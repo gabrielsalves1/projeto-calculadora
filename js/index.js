6969a2b7f51cf8520252b972b2100fea8fc99c5c
Gabriel <gabrielsalves2k18@gmail.com>
2020-06-09 18:00:35 -0300

    Alteração na função calcular no .js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,16 @@
-/* function ligar() {
-    
-
-    if(visor.innerText == "Pressione o botão ON") {
-        visor.innerHTML = "ON";
-    } else if(visor.innerHTML == "ON"){
-        visor.innerHTML = "OFF";
-    }
-} */
 var visor = document.getElementById("visor");
 
 function limpaVisor() {
-    visor.innerHTML = "";
+    visor.innerHTML = '';
 }
 
-function apagarUltimo() {
+function apagarUltimoCaracter() {
     let apagar = visor.innerHTML;
     visor.innerHTML = apagar.substring(0, apagar.length-1);
 }
 
 function obterNumero(num) {
-/*     if(visor.innerHTML == "ON") {
-        limpaVisor();
-    } else if (visor.innerHTML == "OFF"){
-        visor.innerHTML = "Pressione o botão ON";
-    } else if (visor.innerHTML == "Pressione o botão ON" || visor.innerText == "Pressione o botão ON") {
-        visor.innerHTML = "Pressione o botão ON";
-    } */
+    /* Poderia ter passado o value invés do id */
     n = parseFloat(num)
     let numero = 0;
     numero += n;
@@ -49,15 +34,31 @@ function obterOperacao(operacao) {
         case '/':
             visor.innerHTML += '/';
             break;
+        case '%':
+            visor.innerHTML += '%';
+            break;
     }
 }
 
 function calcular() {
     let conta = visor.innerHTML;
+    resultado = eval(conta);
+    visor.innerHTML = resultado;
+
+    /* Função eval não possui porcentagem */
+    let operacaoPorcentagem = conta.indexOf("%") > -1;
+    if(operacaoPorcentagem == true) {
+        let numeros = conta.split('%', 2);
+        let resultado = parseFloat(numeros[0])*(parseFloat(numeros[1]/100));
+        visor.innerHTML = resultado;
+    }
+
+/*     
     let operacaoSoma = conta.indexOf("+") > -1;
     let operacaoSubtracao = conta.indexOf("-") > -1;
     let operacaoMultiplicacao = conta.indexOf("*") > -1;
     let operacaoDivisao = conta.indexOf("/") > -1;
+    let operacaoPorcentagem = conta.indexOf("%") > -1;
     if(operacaoSoma == true){
         let numeros = conta.split('+', 2);
         let resultado = parseFloat(numeros[0])+parseFloat(numeros[1]);
@@ -74,5 +75,10 @@ function calcular() {
         let numeros = conta.split('/', 2);
         let resultado = parseFloat(numeros[0])/parseFloat(numeros[1]);
         visor.innerHTML = resultado;
-    }
-}    
+    } else if(operacaoPorcentagem == true) {
+        let numeros = conta.split('%', 2);
+        let resultado = parseFloat(numeros[0])*(parseFloat(numeros[1]/100));
+        visor.innerHTML = resultado;
+    } 
+*/
+}
